77fa51e7ac66d356be48b4c86df822b0593bff77
vignesh <vigneshbhimala@gmail.com>
2026-05-05 16:47:59 +0530

    bug fixed

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgres://foodapp_db_eg2w_user:vH6HLgjrHfxpA63JdYraceJFkXUGmCP9@dpg-d7sseo3rjlhs73d1q78g-a/foodapp_db_eg2w"
+DATABASE_URL = "postgresql://foodapp_db_eg2w_user:vH6HLgjrHfxpA63JdYraceJFkXUGmCP9@dpg-d7sseo3rjlhs73d1q78g-a/foodapp_db_eg2w"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
