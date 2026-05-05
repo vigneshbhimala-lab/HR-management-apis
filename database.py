611290dc730726ec66d8fd20bd0328eb207632f5
vignesh <vigneshbhimala@gmail.com>
2026-05-05 16:52:38 +0530

    bug fixed

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "postgresql://foodapp_db_eg2w_user:vH6HLgjrHfxpA63JdYraceJFkXUGmCP9@dpg-d7sseo3rjlhs73d1q78g-a/foodapp_db_eg2w"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
